12bcc9a934f7712d85d88e2bb24757d7642d025a
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-09-27 14:40:43 -0400

    the river if statement

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ cross_road = crossroad.lower()
 if cross_road == "right":
     print("There are lions. game over")
 else:
-    river = input("")
+    river = input("There's a river. Do you want to swim or wait? Type 'swim' or wait)
+    river_choice = river.lower()
+    if river_choice == 'swim':
+        print("Game Over! the river is full of crocodiles")
+    else:
+        
