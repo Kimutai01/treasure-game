2b0f5c934011a6f1dd041a422d5881332d7395e9
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-09-27 14:31:14 -0400

    adding the crossroad

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-print("Welcome to the treasure island")
+print("Welcome to the treasure island \nYour mission is to find the treasure")
+crossroad = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'")
+cross_road = crossroad.lower()
