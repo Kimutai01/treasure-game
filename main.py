62601f37d84a209db38c67c4fb3c75cc9192f111
Kimutai01 <kiprotichkimutai01@gmail.com>
2021-09-27 14:36:05 -0400

    the crossroad if statement

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Welcome to the treasure island \nYour mission is to find the treasure")
 crossroad = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'")
 cross_road = crossroad.lower()
+
+if cross_road == "right":
+    print("There are lions. game over")
+else:
+    river = input("")
